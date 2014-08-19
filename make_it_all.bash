0326a85d2e933063ae5e8f6ad3426793c261bc67
John Mark Schofield <jms@schof.org>
2014-08-19 12:52:00 -0700

    wip

--- make_it_all.bash
+++ make_it_all.bash
@@ -7,7 +7,6 @@ source cloudenv
 set -x
 
 ./provision_everything.bash
-# ./config_host_network.bash
 # ./prepare_for_openstack.bash
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --forks=7 -i hosts.txt config_host_packages.yml
 ./wait_for_reboot.bash
